29e8c156f46b892a37abdbbd90f4a144a34a5c9a
tomek <tomekreda16@gmail.com>
2019-04-09 10:07:33 +0200

    https://github.com/TomaszReda/liferay.git

---
+++ modules/untitled/src/main/java/pl/test/constants/pl.testPortletKeys.java
@@ -0,0 +1,10 @@
+package pl.test.constants;
+
+/**
+ * @author tomek
+ */
+public class pl.testPortletKeys {
+
+	public static final String pl.test = "pl.test";
+
+}
---
+++ modules/untitled/src/main/java/pl/test/portlet/pl.testPortlet.java
@@ -0,0 +1,28 @@
+package pl.test.portlet;
+
+import pl.test.constants.pl.testPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author tomek
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + pl.testPortletKeys.pl.test,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class pl.testPortlet extends MVCPortlet {
+}
